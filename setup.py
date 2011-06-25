86ee4495a92f71e18d14023734251b393b18d651
Kazufumi Ohkawa <kerolinq@gmail.com>
2011-06-25 09:34:39 +0900

    edit info

--- setup.py
+++ setup.py
@@ -7,8 +7,38 @@ setup(name='pygamess',
       version=version,
       description="GAMESS wrapper for Python",
       long_description="""\
-GAMESS wrapper for Python""",
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+`pygamess` is a GAMESS wrapper for Python
+
+Requirements
+------------
+* Python 2.6 or later (not support 3.x)
+* openbabel 2.3 
+
+Features
+--------
+* nothing
+
+Setup
+-----
+::
+
+   $ easy_install pygamess
+
+History
+-------
+0.1 (2011-6-25)
+~~~~~~~~~~~~~~~~~~
+* first release
+
+""",
+      classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Scientific/Engineering :: Chemistry',
+        'Development Status :: 2 - Pre-Alpha',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX',
+        'Programming Language :: Python'
+        ], 
       keywords='chemistry',
       author='Ohkawa Kazufumi',
       author_email='kerolinq@gmail.com',
@@ -17,10 +47,5 @@ GAMESS wrapper for Python""",
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests', 'docs']),
       include_package_data=True,
       zip_safe=False,
-      install_requires=[
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
+      requires=['openbabel']
       )
