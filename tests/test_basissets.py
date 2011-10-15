5664955d039c967a68e08ecc52e38df57db70fd3
Kazufumi Ohkawa <kerolinq@gmail.com>
2011-10-15 13:40:11 +0900

    modified test

--- tests/test_basissets.py
+++ tests/test_basissets.py
@@ -1,15 +1,14 @@
+from nose.tools import *
 import sys
-
 sys.path.append("../")
-
 import pygamess
 import openbabel as ob
-g = pygamess.Gamess()
-obc = ob.OBConversion()
-obc.SetInFormat("mol")
-mol = ob.OBMol()
-obc.ReadFile(mol, "../examples/ethane.mol")
 
 def test_ethane_sto3g():
+    obc = ob.OBConversion()
+    obc.SetInFormat("mol")
+    mol = ob.OBMol()
+    obc.ReadFile(mol, "../examples/ethane.mol")
+    g = pygamess.Gamess()
     newmol = g.run(mol)
     assert newmol.GetEnergy() == -78.305307479999996
