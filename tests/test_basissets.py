262b346afdafdb3908b0aa4354d45000efd42620
Kazufumi Ohkawa <kerolinq@gmail.com>
2011-10-15 15:35:56 +0900

    bug: sto3g
    jenkinsでrungmsが動いてないっぽい

--- tests/test_basissets.py
+++ tests/test_basissets.py
@@ -8,7 +8,10 @@ def test_ethane_sto3g():
     mol = ob.OBMol()
     obc.ReadFile(mol, "../examples/ethane.mol")
     g = pygamess.Gamess(gamess_path="/usr/local/bin/rungms")
-    newmol = g.run(mol)
+    try:
+        newmol = g.run(mol)
+    except GamessError, gerr:
+        print gerr.value
 
     eq_(newmol.GetEnergy(),-78.30530748)
 
