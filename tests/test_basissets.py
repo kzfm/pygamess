dde2cf3aa64ed843ae9fb361d42214796e16d166
Kazufumi Ohkawa <kerolinq@gmail.com>
2011-10-15 14:22:05 +0900

    testを変えた
    jenkins対応（そのうちなおす）

--- tests/test_basissets.py
+++ tests/test_basissets.py
@@ -9,7 +9,7 @@ def test_ethane_sto3g():
     obc.SetInFormat("mol")
     mol = ob.OBMol()
     obc.ReadFile(mol, "../examples/ethane.mol")
-    g = pygamess.Gamess()
+    g = pygamess.Gamess(gamess_path="/usr/local/bin/gamess")
     newmol = g.run(mol)
     print newmol.GetEnergy()
     assert newmol.GetEnergy() == -78.305307479999996
