1a36a3b35090b86059e7c08da8672ba64db127f2
Kazufumi Ohkawa <kerolinq@gmail.com>
2012-03-22 06:41:02 +0900

    bugfixed and added document

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.2.0'
+version = '0.2.1'
 
 setup(name='pygamess',
       version=version,
@@ -28,33 +28,100 @@ Setup
 Basic Usage
 -----------
 
-import module and read molecule from file::
+single point calculation with pybel
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    >>> from pygamess import Gamess
+    >>> import pybel
+    >>> g = Gamess()
+    >>> mol = pybel.readstring('smi','C=C')
+    >>> mol.make3D()
+    >>> optimized_mol = g.run(mol)
+    >>> optimized_mol.energy
+    -77.0722784993
+
+single point calculation with openbabel
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     >>> import pygamess
-    >>> import openbabel as ob
     >>> g = pygamess.Gamess()
+    >>> import openbabel as ob
     >>> obc = ob.OBConversion()
     >>> obc.SetInFormat("mol")
     True
     >>> mol = ob.OBMol()
     >>> obc.ReadFile(mol, "examples/ethane.mol")
     True
-
-run GAMESS::
-
     >>> try:
     ...     newmol = g.run(mol)
     ... except GamessError, gerr:
     ...     print gerr.value
     ... 
-
-get energy::
-
     >>> newmol.GetEnergy()
     -78.305307479999996
 
+optimize calculation with pybel
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+set run_type::
+
+    >>> from pygamess import Gamess
+    >>> import pybel
+    >>> g = Gamess()
+    >>> g.run_type('optimize')
+    >>> mol = pybel.readstring('smi','C')
+    >>> mol.make3D()
+    >>> optimized_mol = g.run(mol)
+    >>> optimized_mol.energy
+    -37.0895866208
+
+    >>> g.run_type('optimize')
+    >>> optimized_mol = g.run(mol)
+    >>> optimized_mol.GetEnergy()
+    -78.306179642000004
+
+changing basis-sets
+~~~~~~~~~~~~~~~~~~~
+
+basis_type method::
+
+    >>> g.run_type('energy')
+    >>> g.basis_type('sto3g')
+    {'gbasis': 'sto', 'ngauss': '3'}
+    >>> mol_sto3g = g.run(mol)
+    >>> mol_sto3g.GetEnergy()
+    -78.305307479999996
+    >>> g.basis_type('631g')
+    {'gbasis': 'N31', 'ndfunc': '1', 'ngauss': '6'}
+    >>> mol_631g = g.run(mol)
+    >>> mol_631g.GetEnergy()
+    -79.228127109699997
+    >>> g.basis_type('631gdp')
+    {'gbasis': 'N31', 'ndfunc': '1', 'npfunc': '1', 'ngauss': '6'}
+    >>> mol_631gdp = g.run(mol)
+    >>> mol_631gdp.GetEnergy()
+    -79.237634701499999
+
+or edit property of Gamess instance::
+
+    >>> g.basis = {'gbasis': 'sto', 'ngauss': '3'}
+    >>> mol_sto3g = g.run(mol)
+    >>> mol_sto3g.GetEnergy()
+    -78.305307479999996
+
+print GAMESS INPUT
+~~~~~~~~~~~~~~~~~~
+
+use gamess_input method
+
 History
 -------
+
+0.2.1 (2012-03-23)
+~~~~~~~~~~~~~~~~~~
+* bug fixed (multiplicity setting for pybel) 
+* bug fixed (print error when rungms exec failed)
+* added document
 
 0.2.0 (2012-03-06)
 ~~~~~~~~~~~~~~~~~~
