47e82dd4994987acc5e3b42eb7e876caa5f39f65
Kazufumi Ohkawa <kerolinq@gmail.com>
2012-03-31 08:16:28 +0900

    added tests

--- tests/pygamess_vows.py
+++ tests/pygamess_vows.py
@@ -38,29 +38,40 @@ class PyGamess(Vows.Context):
 
         def should_have_a_contrl(self, topic):
             expect(isinstance(topic.contrl, dict)).to_be_true()
+
         def should_have_a_basis(self, topic):
             expect(isinstance(topic.basis, dict)).to_be_true()
+
         def should_have_a_statpt(self, topic):
             expect(isinstance(topic.statpt, dict)).to_be_true()
+
         def should_have_a_system(self, topic):
             expect(isinstance(topic.system, dict)).to_be_true()
+
         def should_have_a_cis(self, topic):
             expect(isinstance(topic.cis, dict)).to_be_true()
 
         def can_print_section(self, topic):
             expect(topic.print_section).to_be_a_function()
+
         def print_contrl_should_return_a_text(self, topic):
             expect(topic.print_section('contrl')).to_be_like(' $contrl runtyp=energy scftyp=rhf  $end\n')
+
         def print_basis_should_return_a_text(self, topic):
             expect(topic.print_section('basis')).to_be_like(' $basis gbasis=sto ngauss=3 $end\n')
+
         def print_system_should_return_a_text(self, topic):
             expect(topic.print_section('system')).to_be_like(' $system mwords=30  $end\n')
+
         def print_statpt_should_return_a_text(self, topic):
             expect(topic.print_section('statpt')).to_be_like(' $statpt opttol=0.0001 nstep=20  $end\n')
+
         def print_cis_should_return_a_text(self, topic):
             expect(topic.print_section('cis')).to_be_like(' $cis nstate=1  $end\n')
+
         def print_header_should_return_a_text(self, topic):
             expect(topic.print_header()).to_be_like(' $contrl runtyp=energy scftyp=rhf $end\n $basis gbasis=sto ngauss=3  $end\n $system mwords=30  $end\n')
+
 
         class WhenChangedTo631G(Vows.Context):
             def topic(self):
@@ -68,6 +79,14 @@ class PyGamess(Vows.Context):
             def print_basis_can_change_631G(self, topic):
                 topic.basis_set('6-31G')
                 expect(topic.print_section('basis')).to_be_like(' $basis gbasis=N31 ngauss=6  $end\n')
+
+
+        class WhenChangedTo631GDS(Vows.Context):
+            def topic(self):
+                return Gamess()
+            def print_basis_can_change_631G(self, topic):
+                topic.basis_set('6-31G**')
+                expect(topic.print_section('basis')).to_be_like(' $basis gbasis=N31 ndfunc=1 npfunc=1 ngauss=6  $end\n')
 
 
         class WhenRemoveGamess(Vows.Context):
@@ -78,6 +97,7 @@ class PyGamess(Vows.Context):
                 return tmpdir
             def can_remove_tempdir(self, topic):
                 expect(os.path.isdir(topic)).to_be_false()
+
 
         class WhenGivenAObmol(Vows.Context):
             def topic(self):
@@ -100,6 +120,8 @@ H      1.0      1.1404000000   -0.3501000000    0.9626000000
 H      1.0      1.1405000000    1.0087000000   -0.1781000000 
 H      1.0      1.1404000000   -0.6586000000   -0.7845000000 
  $END\n\n\n""")
+
+
         class WhenGivenAPybelmol(Vows.Context):
             def topic(self):
                 return Gamess()
@@ -119,6 +141,8 @@ H      1.0      1.1404000000   -0.3501000000    0.9626000000
 H      1.0      1.1405000000    1.0087000000   -0.1781000000 
 H      1.0      1.1404000000   -0.6586000000   -0.7845000000 
  $END\n\n\n""")
+
+
         class WhenGivenAChargedMol(Vows.Context):
             def topic(self):
                 return Gamess()
@@ -129,12 +153,14 @@ H      1.0      1.1404000000   -0.6586000000   -0.7845000000
             def can_print_gamin_of_cation(self, topic):
                     expect(topic.input(pybel.readstring('smi','[N+]'))).to_be_like(""" $contrl runtyp=energy scftyp=rhf icharg=1 mult=5  $end\n $basis gbasis=sto ngauss=3  $end\n $system mwords=30  $end\n $DATA\n\nC1\nN      7.0      0.0000000000    0.0000000000    0.0000000000 \n $END\n\n\n""")
 
+
         class WhenChangedABasis(Vows.Context):
             def topic(self):
                 return Gamess()
             def should_be_changed_a_basis(self, topic):
                 topic.basis_set('am1')
                 expect(topic.basis).to_be_like({'gbasis':'am1'})
+
 
         class WhenChangedARunType(Vows.Context):
             def topic(self):
@@ -143,6 +169,7 @@ H      1.0      1.1404000000   -0.6586000000   -0.7845000000
                 topic.run_type('optimize')
                 expect(topic.contrl['runtyp']).to_be_like('optimize')
 
+
         class WhenChangedAScf(Vows.Context):
             def topic(self):
                 return Gamess()
@@ -150,11 +177,13 @@ H      1.0      1.1404000000   -0.6586000000   -0.7845000000
                 topic.scf_type('UHF')
                 expect(topic.contrl['scftyp']).to_be_like('UHF')
 
+
         class AfterRunning(Vows.Context):
             def topic(self):
                 return Gamess().run(mol)
             def should_be_new_mol(self, topic):
                 expect(topic.GetEnergy()).to_be_like(-78.30530748)
+
 
         class AfterRunningWithPybel(Vows.Context):
             def topic(self):
